72fcd17025a50c487e4e3536a038c7c51f966184
calgag <calgagi@gmail.com>
2018-09-21 15:20:48 -0700

    Tidy numbers done with the help of Mitchell!

--- tidynumbers/tidynumbers.cpp
+++ tidynumbers/tidynumbers.cpp
@@ -35,11 +35,26 @@ void start(ofstream& file, string line){
 
   string answer = line;
   cout << answer << endl;
-  for(int i = answer.length() - 2; i >= 1; i--){
-    int current = answer[i] - '0';
-    int next = answer[i+1] - '0';
-    if(next > current){
-      answer[i] = answer[i+1];
+  for(int i = 1; i < answer.length() - 1; i++){
+    if(answer[i-1] > answer[i]){
+      answer[i-1]--;
+      for(int j = i; j < answer.length() - 1; j++){
+        answer[j] = '9';
+      }
+      for(int j = i - 1; j >= 1; j--){
+        if(answer[j] < answer[j-1]){
+          answer[j] = '9';
+          answer[j-1]--;
+        }
+      }
+      break;
+    }
+  }
+
+  for(int i = 0; i < answer.length() - 1; i++){
+    if(answer[i] != '0'){
+      answer = answer.substr(i,answer.length()-1);
+      break;
     }
   }
   file << answer << endl;
