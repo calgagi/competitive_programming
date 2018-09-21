e583a0e27ff33ca48ce6fd8835f8abd006a2dcaa
calgag <calgagi@gmail.com>
2018-09-21 13:46:29 -0700

    messed up the string thingy, need to think about it

--- tidynumbers/tidynumbers.cpp
+++ tidynumbers/tidynumbers.cpp
@@ -12,17 +12,11 @@ int main(){
   ifstream file ("B-large-practice.in");
   ofstream outFile ("output.txt");
 
-  // PLAN
-  // Parse number
-  // Keep going down until find perfect number
-  // Use modulus operator
-
   int b = 1;
   if(file.is_open()){
     // filler
     getline(file,line);
     while(getline(file, line)){
-      cout << line << endl;
       outFile << "Case #" << b << ": ";
       start(outFile, line);
       b++;
@@ -34,37 +28,19 @@ int main(){
 }
 
 void start(ofstream& file, string line){
-  // PARSE number
-  long long num = 0;
-  long long b = 1;
-  for(int i = 1; i < line.length() - 1; i++){
-    b = b * 10;
-  }
-  for(int j = 0; j < line.length() - 1; j++){
-    int x = line[j] - '0';
-    num += x * b;
-    b = b / 10;
-  }
+  // Plan:
+  // For the length of the string
+  // Start at back, if number at spot i is less
+  // than the next number, set number to next number
 
-  cout << num << endl;
-  // Number successfully parsed!
-
-
-  long long c = 1;
-  for(int i = 1; i < line.length() - 1; i++){
-    c = c * 10;
-  }
-  long long answer = -1;
-  for(long long i = num; i > 0; i--){
-    string tempi = to_string(i);
-    if(tempi.length() == 1){
-      file << tempi;
-      break;
-    }
-    for(int j = 1; j < tempi.length() - 1; j++){
-      int current = tempi[j] - '0';
-      int previous = tempi[j-1] - '0';
-
+  string answer = line;
+  cout << answer << endl;
+  for(int i = answer.length() - 2; i >= 1; i--){
+    int current = answer[i] - '0';
+    int next = answer[i+1] - '0';
+    if(next > current){
+      answer[i] = answer[i+1];
     }
   }
+  file << answer << endl;
 }
