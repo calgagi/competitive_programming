37061abd155e99bda7355ab672ca6b7d1b01807f
calgag <calgagi@gmail.com>
2018-09-22 21:06:08 -0700

    file in complete

---
+++ bathroomstalls/bathroomstalls.py
@@ -0,0 +1,12 @@
+file = open("C-small-practice-1.in", "r");
+
+line = file.read();
+
+tcs = line.split('\n');
+
+for x in tcs:
+    # Get numbers
+    sp = x.split(' ');
+    stalls = int(sp[0]);
+    people = int(sp[1]);
+    
