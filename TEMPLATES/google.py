b4c55d82ac9c129b898ae77cf8b845b0b5a169c7
calgag <calgagi@gmail.com>
2018-10-20 21:14:15 -0700

    template comments + some things

--- TEMPLATES/google.py
+++ TEMPLATES/google.py
@@ -3,5 +3,6 @@ import sys
 data = True
 
 while data:
-    data = sys.stdin.readline()
+    data = sys.stdin.readline() # read line from stdin
+    data = data.rstrip('\n')  # get rid of newline character
     print(data)
--- google/googlements/googlements.py
+++ google/googlements/googlements.py
@@ -1,1 +1,8 @@
 import sys
+
+data = True
+
+while data:
+    data = sys.stdin.readline() # read line from stdin
+    data = data.rstrip('\n')  # get rid of newline character
+    print(data)
