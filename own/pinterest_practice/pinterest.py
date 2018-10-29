189195426e442f4f0c3a6f01f7e0acd81f86dc2b
calgag <calgagi@gmail.com>
2018-10-28 23:54:40 -0700

    Update own/pinterest_practice/pinterest.py

--- own/pinterest_practice/pinterest.py
+++ own/pinterest_practice/pinterest.py
@@ -6,19 +6,23 @@ def dynamic_programming_func():
     for j in range(1, len(matrix[0])+1):
         for i in range(1, len(matrix)+1):
             max = 0
-            if ord(matrix[i-1][j-1]) - ord(matrix[i-2][j-1]) == 1:
-                if max < mem[i-1][j]:
-                    max = mem[i-1][j]+1
-            if ord(matrix[i-1][j-1]) - ord(matrix[i][j-1]) == 1:
-                if max < mem[i+1][j]:
-                    max = mem[i+1][j]+1
-            if ord(matrix[i-1][j-1]) - ord(matrix[i-1][j]) == 1:
-                if max < mem[i][j+1]:
-                    max = mem[i][j+1]+1
-            if ord(matrix[i][j]) - ord(matrix[i][j-1]) == 1:
-                if max < mem[i][j-1]:
-                    max = mem[i][j-1]+1
+            print(str(i-1) + " " + str(j-1))
+            if j-1-1 > 0:
+                if abs(ord(matrix[i-1][j-1-1]) - ord(matrix[i-1][j-1])) == 1:
+                    if max < mem[i][j-1]:
+                        max = mem[i][j-1]+1
+            if j < len(matrix[0]):
+                if abs(ord(matrix[i-1][j-1+1]) - ord(matrix[i-1][j-1])) == 1:
+                    if max < mem[i][j+1]:
+                        max = mem[i][j+1]+1
+            if i < len(matrix):
+                if abs(ord(matrix[i-1-1][j-1]) - ord(matrix[i-1][j-1])) == 1:
+                    if max < mem[i-1][j]:
+                        max = mem[i-1][j]+1
+            if i-1-1 > 0:
+                if abs(ord(matrix[i-1+1][j-1]) - ord(matrix[i-1][j-1])) == 1:
+                    if max < mem[i+1][j]:
+                        max = mem[i+1][j]+1
             mem[i][j] = max
-    return mem[4][3]
-
-print(dynamic_programming_func())
+    print(mem)
+dynamic_programming_func()
