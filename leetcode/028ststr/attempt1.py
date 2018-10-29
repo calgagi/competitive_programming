fccbe5c5be12867fb44a318a74f99e62409d3bde
calgag <calgagi@gmail.com>
2018-10-29 13:15:10 -0700

    welp almost go 028, not fast enough tho

---
+++ leetcode/028ststr/attempt1.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if len(needle) == 0:
+            return 0
+        # Beginning spots of each potential string
+        pR = []
+        # For the number of elements in haystack, search for those characters
+        for i in range(len(haystack)):
+            if needle[0] == haystack[i]:
+                pR.append(i)
+        # If the next characters match, the string "stays" inside of the pR (list of possible Returns)
+        # If not, remove that element from pR
+        it = 0
+        goodArray = []
+        while len(needle) > it:
+            goodArray = []
+            for i in range(len(pR)):
+                if it < len(needle) and pR[i]+it < len(haystack):
+                    if haystack[pR[i]+it] == needle[it]:
+                        goodArray.append(pR[i])
+            pR = goodArray
+            it += 1
+            if len(pR) == 0:
+                return -1
+        return pR[0]
+
+
+        
--- own/pinterest_practice/pinterest.py
+++ own/pinterest_practice/pinterest.py
@@ -3,26 +3,31 @@ matrix = [['A', 'B', 'H', 'K'], ['C', 'C', 'D', 'G'], ['A', 'B', 'D', 'F']]
 
 def dynamic_programming_func():
     mem = [[0]*(len(matrix[0])+1)]*(len(matrix)+1)
-    for j in range(1, len(matrix[0])+1):
-        for i in range(1, len(matrix)+1):
+    for i in range(1, len(matrix[0])+1):
+        for j in range(1, len(matrix)+1):
             max = 0
-            print(str(i-1) + " " + str(j-1))
-            if j-1-1 > 0:
+            print("====" + str(i-1) + " " + str(j-1) + "=====")
+            print(str(i-1) + " " + str(j-1-1))
+            if j-1-1 > 0 and i-1 >= 0:
                 if abs(ord(matrix[i-1][j-1-1]) - ord(matrix[i-1][j-1])) == 1:
                     if max < mem[i][j-1]:
                         max = mem[i][j-1]+1
-            if j < len(matrix[0]):
+            print(str(i-1) + " " + str(j))
+            if j < len(matrix[0]) and i-1 >= 0:
                 if abs(ord(matrix[i-1][j-1+1]) - ord(matrix[i-1][j-1])) == 1:
                     if max < mem[i][j+1]:
                         max = mem[i][j+1]+1
-            if i < len(matrix):
+            print(str(i-1-1) + " " + str(j-1))
+            if i-1-1 > 0 and j-1 >= 0:
                 if abs(ord(matrix[i-1-1][j-1]) - ord(matrix[i-1][j-1])) == 1:
                     if max < mem[i-1][j]:
                         max = mem[i-1][j]+1
-            if i-1-1 > 0:
+            print(str(i) + " " + str(j-1))
+            if 0 < i < len(matrix[0]) and len(matrix) > j-1 >= 0:
                 if abs(ord(matrix[i-1+1][j-1]) - ord(matrix[i-1][j-1])) == 1:
                     if max < mem[i+1][j]:
                         max = mem[i+1][j]+1
+            print(max)
             mem[i][j] = max
     print(mem)
 dynamic_programming_func()
