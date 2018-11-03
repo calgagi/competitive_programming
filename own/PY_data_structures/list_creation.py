6ca62ec8c2e0260da3d35adf8df7dcb8f12e0d98
calgag <calgagi@gmail.com>
2018-11-02 23:38:39 -0700

    Update own/PY_data_structures/list_creation.py

---
+++ own/PY_data_structures/list_creation.py
@@ -0,0 +1,11 @@
+squares = [x**2 for x in range(10)]
+print(squares)
+
+zero_matrix = [[0 for x in range(10)] for y in range(10)]
+print(zero_matrix)
+
+init_array = [0]*10
+print(init_array)
+
+coords = [(x,y) for x in [1, 2, 3] for y in [1, 2, 3]]
+print(coords)
