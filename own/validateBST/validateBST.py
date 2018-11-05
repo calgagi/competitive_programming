a67d5e5eadaa80bb3a7cfd6b75a06618c97da39d
calgag <calgagi@gmail.com>
2018-11-05 08:01:49 -0800

    some stuff

---
+++ own/validateBST/validateBST.py
@@ -0,0 +1,13 @@
+class TreeNode:
+    val = 0
+    TreeNode left = None
+    TreeNode right = None
+
+def inOrder(root, test):
+    if root == None:
+        return
+    inOrder(root.left, test)
+    test.append(root.val)
+    inOrder(root.right, test)
+
+def 
