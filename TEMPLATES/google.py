0aedcf166800805e64a27a31c5ae5c65a7952142
calgag <calgagi@gmail.com>
2018-12-03 21:35:12 -0800

    deleted templates

--- TEMPLATES/google.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-data = True
-
-while data:
-    data = sys.stdin.readline() # read line from stdin
-    data = data.rstrip('\n')  # get rid of newline character
-    print(data)
--- TEMPLATES/icpc.py
+++
@@ -1,3 +0,0 @@
-import sys
-
-data = sys.stdin.readline()
