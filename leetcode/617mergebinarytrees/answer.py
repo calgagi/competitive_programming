50dd92d0ccbeb399d492ee5b3d2f2ca5b35c45c1
calgag <calgagi@gmail.com>
2018-12-23 21:58:04 -0800

    Create answer.py

---
+++ leetcode/617mergebinarytrees/answer.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        if t1 == None and t2 == None:
+            return None
+        if t1 != None and t2 != None:
+            t1.val = t1.val + t2.val
+            t1.right = self.mergeTrees(t1.right, t2.right)
+            t1.left = self.mergeTrees(t1.left, t2.left)
+            return t1
+        elif t1 == None:
+            return t2
+        elif t2 == None:
+            return t1
