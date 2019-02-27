504942fa9ced8780f00f0fd71e532322beea0cc5
calgagi <calgagi@gmail.com>
2019-02-26 19:32:36 -0800

    230

---
+++ leetcode/230kthsmallestbst/answer.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        stack = []
+        
+        while len(stack) != 0 or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            
+            root = stack.pop()
+            k -= 1
+            
+            if k == 0:
+                return root.val
+            
+            root = root.right
