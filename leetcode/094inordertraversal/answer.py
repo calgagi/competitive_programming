ec6277c69344e30dc25cc77c67e077d2788f31cf
calgagi <calgagi@gmail.com>
2019-02-26 18:50:19 -0800

    094

---
+++ leetcode/094inordertraversal/answer.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        r = []
+        if root.left:
+            r += self.inorderTraversal(root.left)
+        r += [root.val]
+        if root.right:
+            r += self.inorderTraversal(root.right)
+        return r
+        
