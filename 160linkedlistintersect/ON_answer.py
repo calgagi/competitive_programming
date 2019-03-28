be276ee86cb441f14ef0e141d2f3a880262ed2df
calgagi <calgagi@gmail.com>
2019-03-28 16:37:45 -0700

    160

---
+++ 160linkedlistintersect/ON_answer.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        itA, itB = headA, headB
+        seen = {}
+        while itA:
+            if itA not in seen:
+                seen[itA] = 1
+            itA = itA.next
+        while itB:
+            if itB in seen:
+                return itB
+            itB = itB.next
+        return None
+        
