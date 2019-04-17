ba7fcc57d96dbab2ad5f7863f3ffef34bc09e0e3
calgagi <calgagi@gmail.com>
2019-04-17 13:54:18 -0700

    0322

---
+++ leetcode/0322coinchange/answer.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [0] + [float('inf')]*amount
+        
+        for i in range(1, len(dp)):
+            for c in coins:
+                if i - c < len(dp):
+                    dp[i] = min(dp[i], dp[i-c]+1)
+                    
+        if dp[amount] == float('inf'):
+            return -1
+        return dp[amount]
+                    
+        
