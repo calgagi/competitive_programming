5acb5e3df9069e9637173952a41cd6f6e5847f75
calgagi <calgagi@gmail.com>
2019-05-25 14:47:51 -0700

    0946

---
+++ leetcode/0946/answer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> s;
+        int j = 0;
+        for (int i = 0; i < pushed.size() && j < popped.size(); i++) {
+            s.push(pushed[i]);
+            while (!s.empty() && j < popped.size() && s.top() == popped[j]) {
+                s.pop();
+                j++;
+            }
+        }
+        return j == popped.size();
+    }
+};
