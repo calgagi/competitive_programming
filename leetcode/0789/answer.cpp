699f80f7f25217edf39744eea4778bd2bb4c904f
calgagi <calgagi@gmail.com>
2019-06-20 21:48:44 -0700

    0789

---
+++ leetcode/0789/answer.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool escapeGhosts(vector<vector<int>>& ghosts, vector<int>& target) {
+        int d = abs(target[0]) + abs(target[1]);
+        for (int i = 0; i < ghosts.size(); i++) {
+            if (abs(ghosts[i][0]-target[0])+abs(ghosts[i][1]-target[1]) <= d)
+                return false;
+        }
+        return true;
+    }
+};
