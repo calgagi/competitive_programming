aa96ee200a54ac43ec450e6e04796fbdda20bf7a
calgagi <calgagi@gmail.com>
2019-07-20 00:37:57 -0700

    688, hard to do

--- leetcode/0688/answer.cpp
+++ leetcode/0688/answer.cpp
@@ -1,59 +1,22 @@
 class Solution {
-
 public:   
-
     double knightProbability(int N, int K, int r, int c) {
-
-        int **dp0 = new int*[N];
-
-        for (int i = 0; i < N; i++) dp0[i] = new int[N];
-
-        int moves[8][2] = {{2, 1}, {-2, 1}, {2, -1}, {-2, -1}, {1, 2}, {-1, 2}, {1, -2}, {-1, -2}};
-
-        for (int i = 0; i < N; i++)
-
+        double dp[K+1][N][N];
+        double moves[8][2] = {{2,1}, {1,2}, {-2,1}, {-1,2}, {-2,-1}, {-1,-2}, {2,-1}, {1,-2}};
+        for (int i = 0; i <= K; i++)
             for (int j = 0; j < N; j++)
-
-                dp0[i][j] = 1;
-
-        dp0[r][c] = 1;
-
-        int x = 0;
-
-        for (int k = 0; k < K; k++) {
-
-            int **dp1 = new int*[N];
-
-            for (int i = 0; i < N; i++) dp1[i] = new int[N];  
-
-            for (int i = 0; i < N; i++) {
-
-                for (int j = 0; j < N; j++) {
-
-                    dp1[i][j] = 0;
-
+                for (int k = 0; k < N; k++)
+                    dp[i][j][k] = i == 0 ? 1.0 : 0.0;
+        for (int i = 1; i <= K; i++)
+            for (int j = 0; j < N; j++)
+                for (int k = 0; k < N; k++)
                     for (int m = 0; m < 8; m++) {
-
-                        int row = i + moves[m][0];
-
-                        int col = j + moves[m][1];
-
+                        int row = j + moves[m][0];
+                        int col = k + moves[m][1];
                         if (row < N && col < N && row >= 0 && col >= 0)
-
-                            dp1[i][j] += dp0[row][col]; 
-
-                    }
-
-                }
-
-            }
-
-            dp0 = dp1;
-
-        }
-
-        return (double) dp0[r][c] / pow(8, K); 
-
+                            dp[i][j][k] += dp[i-1][row][col];
+                    } 
+                   
+        return dp[K][r][c] / pow(8, K); 
     }
-
 };
