5993862e5eb705a116b6d6d0a7e95057ba61d4a1
calgagi <calgagi@gmail.com>
2019-07-25 22:36:07 -0700

    turns out 805A is super easy, one if statement???

--- comp/codeforces/0805A/template.cpp
+++ comp/codeforces/0805A/template.cpp
@@ -13,33 +13,14 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    // Calculate primes using Sieve of Eratosthenes
-    int r, l;
+    int l, r;
     cin >> l >> r;
-    bool *prime = new bool[r+1];
-    memset(prime, 1, sizeof(bool)*(r+1));
-    prime[0] = prime[1] = false;
-    int s = sqrt(r);
-    vector<int> primes;
-    for (int i = 2; i <= r; i++) {
-        if (prime[i]) {
-            primes.push_back(i);
-            for (int k = i*i; k <= r; k += i)
-                prime[k] = false;
-        }
+    if (l == r) {
+        cout << l << endl;
+    } else {
+        cout << 2 << endl;
     }
 
-    vector<int> counts(primes.size(), 0);
-    int x = -1, v = 0;
-    for ( ; l <= r; l++) {
-        int t = 0;
-        while (l % primes[t]) t++;
-        counts[t]++;
-        if (x < counts[t]) { 
-            x = counts[t];
-            v = primes[t]; 
-        }
-    }
-    cout << v << endl;
+
     return 0;
 }
