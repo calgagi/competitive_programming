0badc16bd41f6788f6c3f89655fda42f37848fca
calgagi <calgagi@gmail.com>
2019-11-21 23:00:40 -0800

    D from ICPC pacnw 2019

---
+++ comp/icpc/pacnw2019/D.cpp
@@ -0,0 +1,35 @@
+// Calvin Gagliano
+// Oregon State University
+// Competitive Programming Template
+#include <bits/stdc++.h>
+
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define x first
+#define y second
+#define pb push_back
+#define forn(i, n) for(int i = 0; i < int(n); ++i)
+#define fore(i, l, r) for(int i = int(l); i < int(r); ++i)
+#define forb(i, n) for(int i = int(n) - 1; i >= 0; --i)
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    srand(chrono::steady_clock::now().time_since_epoch().count());
+	
+    ll A, B;
+    cin >> A >> B;
+    ll res = 0;
+    while(A != B) {
+        if (A % 2 == 1) A++;
+        else if (A > B) A /= 2;
+        else A++;
+        res++;
+    }
+    cout << res << endl;
+
+    return 0;
+}
