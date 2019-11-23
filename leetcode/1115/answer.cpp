c0b02e5a0b82c45867dce0b2067895374d2f2888
calgagi <calgagi@gmail.com>
2019-11-23 15:46:40 -0800

    lc 1115 and 1227

---
+++ leetcode/1115/answer.cpp
@@ -0,0 +1,31 @@
+class FooBar {
+private:
+    int n;
+    mutex a, b;
+
+public:
+    FooBar(int n) {
+        this->n = n;
+        b.lock();
+    }
+
+    void foo(function<void()> printFoo) {
+        
+        for (int i = 0; i < n; i++) {
+            a.lock();
+        	// printFoo() outputs "foo". Do not change or remove this line.
+        	printFoo();
+            b.unlock();
+        }
+    }
+
+    void bar(function<void()> printBar) {
+        
+        for (int i = 0; i < n; i++) {
+            b.lock();
+        	// printBar() outputs "bar". Do not change or remove this line.
+        	printBar();
+            a.unlock();
+        }
+    }
+};
