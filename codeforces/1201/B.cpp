4517f0335b41743f111697c7f74989b3a946adde
calgagi <calgagi@gmail.com>
2019-12-18 20:40:39 -0800

    1201 B: simple

---
+++ codeforces/1201/B.cpp
@@ -0,0 +1,43 @@
+// Calvin Gagliano 
+// @calgagi
+// Oregon State University
+// Competitive Programming Template
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define F first
+#define S second
+#define pb push_back
+#define forn(i, n) for(int i = 0; i < int(n); ++i)
+#define fore(i, l, r) for(int i = int(l); i < int(r); ++i)
+#define forb(i, n) for(int i = int(n) - 1; i >= 0; --i)
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+template<class T>
+inline std::ostream& operator<<(ostream& os, const vector<T>& v) { os << "["; for (auto ii = v.begin(); ii != v.end(); ++ii) { os << " " << *ii; } os << "]"; return os; }
+template<class T1, class T2>
+inline std::ostream& operator<<(ostream& os, const pair<T1, T2>& p) { os << "["; os << p.F << "," << p.S; os << "]"; return os; }
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    srand(chrono::steady_clock::now().time_since_epoch().count());
+
+    int n; cin >> n;
+    ll oe = 0, in = 0, mx = 0;
+    forn(i, n) {
+        cin >> in;
+        oe += in;
+        mx = max(mx, in);
+    }
+    if (oe % 2 == 0 && mx <= oe/2)
+       cout << "YES" << endl;
+    else
+       cout << "NO" << endl; 
+
+
+    return 0;
+}
