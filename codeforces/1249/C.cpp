8dba340ee9dc0dbb9d485800eb860e28b91f70c6
calgagi <calgagi@gmail.com>
2019-12-18 21:44:45 -0800

    1249C cf, tricky problem

--- codeforces/1249/C.cpp
+++ codeforces/1249/C.cpp
@@ -15,15 +15,45 @@
 
 using namespace std;
 
+vector<ll> p;
+
+
+void solve() {
+    ll n; cin >> n;
+    // Greedy?
+    ll res = 0, copy = n;
+    for (int i = 38; i >= 0 && res < n; i--) {
+        if (copy < p[i]/2) continue;
+        if (p[i] == copy) {
+            res += p[i];
+        }
+        if (p[i]/2 == copy) {
+            res += p[i]/2;
+        }
+        if (copy < p[i] && copy > p[i]/2) {
+            res += p[i];
+        }
+        if (copy > p[i]) {
+            res += p[i];
+            copy -= p[i];
+        }
+    }
+        
+
+    cout << res << endl;
+    return;
+}
+
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 	
     long q; cin >> q;
-    forn(j, q) {
-        ll i; cin >> i;
-        
-    }
+    p.resize(39);
+    p[0] = 1;
+    fore(i, 1, 39) p[i] = (ull) 3 * p[i-1];
+    while (q--) solve();
 
     return 0;
 }
