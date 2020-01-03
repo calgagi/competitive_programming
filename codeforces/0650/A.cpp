601caef7e493269cffad5c6aff67bc960eb17c8b
calgagi <calgagi@gmail.com>
2020-01-02 19:58:08 -0800

    Watchmen problem again. cf 650A

--- codeforces/0650/A.cpp
+++ codeforces/0650/A.cpp
@@ -1,42 +1,32 @@
-// Calvin Gagliano
+// Calvin Gagliano 
+// @calgagi
 // Oregon State University
 // Competitive Programming Template
 #include <bits/stdc++.h>
 
-#define ull unsigned long long
-#define ll long long
-#define ld long double
-
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    srand(chrono::steady_clock::now().time_since_epoch().count());
 
+    // The only time manhattan == regular is when y = y or x = x?
+    long long ans = 0;
     int n; cin >> n;
-    unordered_map<int,int> xs;
-    unordered_map<int,int> ys;
-    unordered_map<int, unordered_map<int,int> > ps;
-    ull r = 0;
+    unordered_map<int,int> x;
+    unordered_map<int,int> y;
+    unordered_map<int,unordered_map<int,int>> same;
     for (int i = 0; i < n; i++) {
-        int x, y;
-        cin >> x >> y;
-        if (!ps.count(x))
-            ps[x] = unordered_map<int,int>();
-        if (!ps[x].count(y))
-            ps[x][y] = 0;
-        if (!xs.count(x))
-            xs[x] = 0;
-        if (!ys.count(y))
-            ys[y] = 0;
-        ps[x][y]++;
-        xs[x]++;
-        ys[y]++;
-        int t = (xs[x]-1) + (ys[y]-1) - (ps[x][y]-1);
-        r += t;
+        int a, b; cin >> a >> b;
+        ans += x[a] + y[b] - same[a][b];
+        x[a]++;
+        y[b]++;
+        same[a][b]++;
     }
-    cout << r << endl;
 
+    cout << ans << endl;
 
     return 0;
 }
