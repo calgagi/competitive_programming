45ec5c79ba4e071255270b79ca721e5e46de53ea
calgagi <calgagi@gmail.com>
2020-07-19 00:00:08 -0700

    test

---
+++ codeforces/1372/D.cpp
@@ -0,0 +1,43 @@
+/*
+ * author: calgagi
+ *         Calvin Gagliano 
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define long long long
+#define double double double
+#define ulong unsigned long long
+#define ii pair<int, int>
+#define ll pair<long, long>
+#define ar array
+#define f first
+#define s second
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+        
+
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);  
+    cin.tie(NULL);
+    srand(chrono::steady_clock::now().time_since_epoch().count()); 
+    
+    int t = 1;
+    /* cin >> t; */
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
