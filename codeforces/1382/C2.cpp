505a7de336df781c5c477c9c732a022bc8384b54
calgagi <calgagi@gmail.com>
2020-07-23 10:54:16 -0700

    c2 from contest

--- codeforces/1382/C2.cpp
+++ codeforces/1382/C2.cpp
@@ -21,21 +21,37 @@ void solve() {
     string a, b;
     cin >> a >> b;
 
-    vector<int> ans;
-    bool flip = 0;
-    for (int i = 0; i < n; i++) {
-        if (!flip && a[i] == b[n-i-1]) {
-            ans.push_back(0);
-        } else if (flip && a[n-i-1] != b[n-i-1]) {
-            ans.push_back(0);
+    vector<int> ansA, ansB;
+    if (a[0] != '0') {
+        ansA.push_back(0);
+    } 
+    if (b[0] != '0') {
+        ansB.push_back(0);
+    }
+    a[0] = b[0] = '0';
+
+    bool flipA = 0, flipB = 0;
+    for (int i = 1; i < n; i++) {
+        if (a[i] != a[i-1]) {
+            ansA.push_back(i-1);
+            flipA = !flipA;
         }
-        flip = !flip;
-        ans.push_back(n-i-1);
+        if (b[i] != b[i-1]) {
+            ansB.push_back(i-1);
+            flipB = !flipB;
+        }
     }
 
-    cout << ans.size() << " ";
-    for (int i = 0; i < (int) ans.size(); i++) {
-        cout << ans[i]+1 << " ";
+    if (flipA != flipB) {
+        ansB.push_back(n-1);
+    }
+
+    cout << ansA.size() + ansB.size() << " ";
+    for (int i = 0; i < (int) ansA.size(); i++) {
+        cout << ansA[i]+1 << " ";
+    }
+    for (int i = ansB.size()-1; i >= 0; i--) {
+        cout << ansB[i]+1 << " ";
     }
     cout << endl;
 
